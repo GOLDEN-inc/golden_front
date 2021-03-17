f64c14348927f73ae04fc9c0c410c5c8682d3468
Igor Lima <igorlima1740@gmail.com>
2021-03-17 20:56:52 -0300

    ciração de página de profile em andamento e remoção de biblio de QR code

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
     Container,
@@ -8,15 +8,21 @@ import {
     FormFeedback,
     Input,
     Row,
+    Label,
 } from "reactstrap";
 
 import Nav from "../Navbar/Nav";
+import QRCode from "qrcode.react";
 
 import golden from "../../images/golden_baby.jpg";
 
 import "./Profile.css";
 
 const Profile = () => {
+    const [goldenInput, setGoldenInput] = useState("");
+
+    const editProfile = () => {};
+
     return (
         <React.Fragment>
             <Nav />
@@ -33,14 +39,62 @@ const Profile = () => {
                     </Col>
                 </Row>
                 <Row>
-                    <Col>.col</Col>
-                    <Col>.col</Col>
+                    <Container className="editar-button">
+                        <button
+                            className="button1 button1-profile"
+                            onClick={(e) => editProfile()}
+                        >
+                            Salvar informações
+                        </button>
+                    </Container>
                 </Row>
                 <Row>
-                    <Col>.col</Col>
+                    <Col>
+                        <Form>
+                            <FormGroup>
+                                <Container className="golden-container">
+                                    <Label>GOLDEN</Label>
+                                    <Input
+                                        type="text"
+                                        placeholder="Daphne"
+                                        onChange={(e) =>
+                                            setGoldenInput(e.target.value)
+                                        }
+                                    />
+                                </Container>
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                    <Col>
+                        <Container className="qrcode-container">
+                            <QRCode value={goldenInput} />
+                        </Container>
+                    </Col>
+                </Row>
+                <hr className="profile-hr" />
+                <Row>
+                    <Col>
+                        <Container>
+                            <Form>
+                                <FormGroup>
+                                    <Container>
+                                        <Label className="label-center">
+                                            PIX
+                                        </Label>
+                                        <Input
+                                            type="text"
+                                            placeholder="XXX.XXX.XXX-XX"
+                                        />
+                                    </Container>
+                                </FormGroup>
+                            </Form>
+                        </Container>
+                    </Col>
                 </Row>
                 <Row>
-                    <Col>.col</Col>
+                    <Col>
+                        <Label className="label-center">Posts</Label>
+                    </Col>
                 </Row>
             </Container>
         </React.Fragment>
