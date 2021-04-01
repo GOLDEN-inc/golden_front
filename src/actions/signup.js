e79a37dd28b9a2f5d487d580ad68d9e3254903d5
Igor Lima <igorlima1740@gmail.com>
2021-04-01 17:20:41 -0300

    refatorando código de signup

--- src/actions/signup.js
+++ src/actions/signup.js
@@ -1,31 +1,13 @@
-import firebase from "../firebase/config";
-import axios from "axios"
+// import firebase from "../firebase/config";
+import backendService from "../backendService"
 
 export const createUser = (email, password, name, golden, pix, telfone_wpp) => {
     return async function (dispatch) {
-
-        const user = {
-            name,
-            golden,
-            email,
-            password,
-            pix,
-            telfone_wpp
-
-        }
+        // dispatch function for creating a new user
 
         // const user = await firebase.signup(email, password); //! Code to user firebase
-        const userFetch = fetch("http://localhost:8081/signup", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(user)
-        }).then(response => {
-            return response.json()
-        }).catch(err => console.log(err))
+        const user = await backendService.signup(email, password, name, golden, pix, telfone_wpp)
         console.log(user);
-        dispatch({type: "CREATE_USER", payload: userFetch});
+        dispatch({type: "CREATE_USER", payload: user});
     };
 };
---
+++ src/backendService/index.js
@@ -0,0 +1,26 @@
+class BackEnd {
+    async signup(email, password, name, golden, pix, telfone_wpp) {
+
+        const user = {
+            name,
+            golden,
+            email,
+            password,
+            pix,
+            telfone_wpp
+        }
+
+        return fetch("http://localhost:8081/signup", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        }).then(response => {
+            return response.json()
+        }).catch(err => console.log(err))
+    }
+}
+
+export default new BackEnd()
