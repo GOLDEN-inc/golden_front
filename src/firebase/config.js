5d8b600564477725698458c7fb7d75c1aacd34e8
Igor Lima <igorlima1740@gmail.com>
2021-04-27 22:58:12 -0300

    removed apikey from firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdtZIodtPmVK9XUiVY1VEJYIYO-CPf_ts',
-  authDomain: 'golden-ef7d8.firebaseapp.com',
-  projectId: 'golden-ef7d8',
-  storageBucket: 'golden-ef7d8.appspot.com',
-  messagingSenderId: '737929226078',
-  appId: '1:737929226078:web:195f7f2b78b79b031a91cf',
-  measurementId: 'G-9S6SWXRQ90',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 class Firebase {
