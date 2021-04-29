ffb7a3678b4b79efd4cfbbcfb4de136c8f93a186
Igor Lima <igorlima1740@gmail.com>
2021-04-29 17:06:52 -0300

    code refactoring - folders have been deleted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import Store from './store/store';
-import { Provider } from 'react-redux';
 
 import Routes from './routes';
 import './App.css';
 
 function App() {
   return (
-    <Provider store={Store}>
-      <div className="App">
-        <Routes />
-      </div>
-    </Provider>
+    <div className="App">
+      <Routes />
+    </div>
   );
 }
 
--- src/actions/create.js
+++
@@ -1,8 +0,0 @@
-import firebase from '../firebase/config';
-
-export const createPost = (post) => {
-  return async function (dispatch) {
-    const firestorePost = await firebase.createPost(post);
-    dispatch({ type: 'CREATE_POST', payload: firestorePost });
-  };
-};
